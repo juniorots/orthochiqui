f41654dc3e58fe1298cdac1fbc9bb443984a0777
juniorots <juniorots@gmail.com>
2019-10-29 23:12:11 -0300

    Tratando desenvolvimento site Dentistas [ ORTHO CHIQUI ]

---
+++ montarCommit.sh
@@ -0,0 +1,9 @@
+git add README.md
+git add apps.js/*
+git add models/*
+git add package.json/*
+git public/*
+git routes/*
+git add montarCommit.sh
+
+git commit -m "Tratando desenvolvimento site Dentistas [ ORTHO CHIQUI ]"
