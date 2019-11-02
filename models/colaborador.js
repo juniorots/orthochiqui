42ff7484f9ae4cb22f07b40e45baad363cb1abb7
juniorots <juniorots@gmail.com>
2019-11-02 00:17:03 -0300

    Tratando desenvolvimento site Dentistas [ ORTHO CHIQUI ]

--- models/colaborador.js
+++ models/colaborador.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var colaboradorSchema = new mongoose.Schema({
   nome: String,
   cro: String,
-  funcao: String  
-  telefone: {type: Number, default: 0},
+  funcao: String,
+  telefone: {type: Number, default: 0}
 });
  
 mongoose.model('colaborador', colaboradorSchema);
