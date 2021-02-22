ba26b0fb11cfdd8a21ed4ccca87ff8475fe9edd8
juniorots <juniorots@gmail.com>
2021-02-22 10:42:17 -0300

    Manutencao DENTISTAS [ ORTHO CHIQUI ]

--- montarCommit.sh
+++ montarCommit.sh
@@ -6,4 +6,4 @@ git add public/*
 git add routes/*
 git add montarCommit.sh
 
-git commit -m "Tratando desenvolvimento site Dentistas [ ORTHO CHIQUI ]"
+git commit -m "Manutencao DENTISTAS [ ORTHO CHIQUI ]"
